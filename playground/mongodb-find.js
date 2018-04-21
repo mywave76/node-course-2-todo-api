27cb85d4bc081796056652472a80a9aa19d7bef8
My Wave <mywave.reloaded@yahoo.com>
2018-04-22 00:17:13 +0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5adb623984ce7e5b0272aa0a')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  // db.close();
+});
